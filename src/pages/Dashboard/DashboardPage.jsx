7e98cc676746f1e015b456f8330d148d21f3026e
Goku216 <sagarpariyar345@gmail.com>
2024-09-12 16:15:41 -0400

    data service API managed

--- src/pages/Dashboard/DashboardPage.jsx
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -1,26 +1,15 @@
 import { useEffect, useState } from "react";
 import { DashboardCard } from "./components/DashboardCard";
 import { DashboardEmpty } from "./components/DashboardEmpty";
+import { getUserOrders } from "../../services";
 
 export const DashboardPage = () => {
-  const token = JSON.parse(sessionStorage.getItem("token"));
-  const cbid = JSON.parse(sessionStorage.getItem("cbid"));
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     async function fetchOrders() {
-      const response = await fetch(
-        `http://localhost:8000/660/orders?user.id=${cbid}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      const data = await getUserOrders();
 
-      const data = await response.json();
       setOrders(data);
     }
     fetchOrders();
--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,1 +1,2 @@
 export { login, register, logout } from "./authServices";
+export { getUser, getUserOrders, createOrder } from "./dataService";
